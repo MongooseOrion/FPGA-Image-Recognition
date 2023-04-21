4c509d49202765609d6c1a63e0b289799fdec6ef
Mongoose <smn9008@outlook.com>
2023-04-21 17:01:11 +0800

    v0.1.07

--- RTL/ahb_to_axi_bridge.v
+++ RTL/ahb_to_axi_bridge.v
@@ -90,6 +90,7 @@ reg         axi_bid;
 reg         axi_bresp;
 reg         axi_hready;
 reg         axi_hresp;
+reg [127:0] axi_hrdata;
 
 reg         axi_arvalid_dg;
 reg         axi_awvalid_dg;
@@ -142,6 +143,7 @@ assign axi_arid_dl = (hsel == 2'b00) ? 2'b00 : 2'b01;
 
 assign  hready = axi_hready;
 assign  hresp = axi_hresp;
+assign  hrdata = axi_hrdata;
 assign  arburst = axi_arburst;
 assign  arid = axi_arid;
 assign  arvalid = axi_arvalid;
@@ -149,20 +151,18 @@ assign  awaddr = axi_awaddr;
 assign  awburst = axi_awburst;
 assign  awvalid = axi_awvalid;
 assign  awid = axi_awid;
-//assign  rdata = axi_rdata;
-//assign  bresp = axi_bresp;
 assign  wdata = axi_wdata;
 //assign  wready = axi_wready;
 assign  wvalid = axi_wvalid;
 assign  wstrb = axi_wstrb;
 assign  rready = axi_rready;
-assign  rresp = axi_rresp;
+//assign  rresp = axi_rresp;
 //assign  rvalid = axi_rvalid;
 
 
 
-always @(posedge clk) begin
-    if (reset) begin
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
         axi_arvalid <= 1'b0;
         axi_awvalid <= 1'b0;
         axi_wvalid <= 1'b0;
@@ -192,16 +192,16 @@ always @(posedge clk) begin
                     axi_wvalid <= 1'b1;
         end
         default : begin
-                    axi_arvalid <= axi_arvalid;
-                    axi_awvalid <= axi_awvalid;
-                    axi_wvalid <= axi_wvalid;
+                    axi_arvalid <= 1'b0;
+                    axi_awvalid <= 1'b0;
+                    axi_wvalid <= 1'b0;
         end
         endcase
     end
 end
 
-always @(posedge clk) begin
-    if (reset) begin
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
         axi_wdata <= 16'h0;
         axi_wstrb <= 16'h0;
     end 
@@ -216,8 +216,8 @@ always @(posedge clk) begin
     end
 end
 
-always @(posedge clk) begin
-    if (reset) begin
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
         axi_hready <= 1'b0;
     end 
     else begin
@@ -241,14 +241,14 @@ always @(posedge clk) begin
                 axi_hready <= (arready & awready & 1'b1);
         end
         default : begin
-                axi_hready <= axi_hready;
+                axi_hready <= 1'b0;
         end
         endcase
     end
 end
 
-always @(posedge clk) begin
-    if (reset) begin
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
         axi_arvalid_dg <= 1'b0;
         axi_awvalid_dg <= 1'b0;
         axi_wvalid_dg <= 1'b0;
@@ -259,105 +259,125 @@ always @(posedge clk) begin
         axi_wvalid_dg <= axi_wvalid;
     end
 end
-
-always @(posedge clk) begin
-    if (reset) begin
+/*
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
         axi_arready <= 1'b0;
         axi_awready <= 1'b0;
-        axi_wready <= 1'b0;
+        //axi_wready <= 1'b0;
     end 
     else begin
         axi_arready <= (axi_arvalid_dl & hready & (axi_rready | !axi_rvalid));
         axi_awready <= (axi_awvalid_dl & hready & (axi_bready | !axi_bvalid));
-        axi_wready <= (axi_wvalid_dl & hready);
+        //axi_wready <= (axi_wvalid_dl & hready);
     end
 end
-
-always @(posedge clk) begin
-    if (reset) begin
-        axi_rvalid <= 1'b0;
+*/
+/*
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
+        //axi_rvalid <= 1'b0;
         axi_bvalid <= 1'b0;
-        axi_rresp <= 2'b00;
+        //axi_rresp <= 2'b00;
         axi_bid <= 2'b00;
     end 
     else begin
         case ({axi_arvalid_dl, axi_awvalid_dl, axi_rvalid_dl, axi_bvalid_dl})
         4'b0001 : begin // AR only
-                    axi_rvalid <= arready & axi_arvalid_dl & axi_rready;
+                    //axi_rvalid <= arready & axi_arvalid_dl & axi_rready;
                     axi_bvalid <= axi_bvalid_dl;
-                    axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
+                    //axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
                     axi_bid <= axi_bid;
         end
         4'b0010 : begin // AW only
-                    axi_rvalid <= axi_rvalid_dl;
+                    //axi_rvalid <= axi_rvalid_dl;
                     axi_bvalid <= awready & axi_awvalid_dl & axi_bready;
-                    axi_rresp <= axi_rresp;
+                    //axi_rresp <= axi_rresp;
                     axi_bid <= (axi_bvalid) ? axi_awid : axi_bid;
         end
         4'b0100 : begin // R only
-                    axi_rvalid <= axi_rvalid_dl;
+                    //axi_rvalid <= axi_rvalid_dl;
                     axi_bvalid <= axi_bvalid_dl;
-                    axi_rresp <= axi_rresp;
+                    //axi_rresp <= axi_rresp;
                     axi_bid <= axi_bid;
         end
         4'b1000 : begin // B only
-                    axi_rvalid <= axi_rvalid_dl;
+                    //axi_rvalid <= axi_rvalid_dl;
                     axi_bvalid <= axi_bvalid_dl;
-                    axi_rresp <= axi_rresp;
+                    //axi_rresp <= axi_rresp;
                     axi_bid <= axi_bid;
         end
         4'b0011 : begin // AW and R
-                    axi_rvalid <= arready & axi_arvalid_dl & axi_rready;
+                    //axi_rvalid <= arready & axi_arvalid_dl & axi_rready;
                     axi_bvalid <= axi_bvalid_dl;
-                    axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
+                    //axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
                     axi_bid <= axi_bid;
         end
         4'b0101 : begin // AR and R
-                    axi_rvalid <= arready & axi_arvalid_dl & axi_rready;
+                    //axi_rvalid <= arready & axi_arvalid_dl & axi_rready;
                     axi_bvalid <= axi_bvalid_dl;
-                    axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
+                    //axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
                     axi_bid <= axi_bid;
         end
         4'b0110 : begin // AR and AW
-                    axi_rvalid <= axi_rvalid_dl;
+                    //axi_rvalid <= axi_rvalid_dl;
                     axi_bvalid <= awready & axi_awvalid_dl & axi_bready;
-                    axi_rresp <= axi_rresp;
+                    //axi_rresp <= axi_rresp;
                     axi_bid <= (axi_bvalid) ? axi_awid : axi_bid;
         end
         4'b0111 : begin // AR, AW, and R
-                    axi_rvalid <= arready & awready & axi_arvalid_dl & axi_rready;
+                    //axi_rvalid <= arready & awready & axi_arvalid_dl & axi_rready;
                     axi_bvalid <= axi_bvalid_dl;
-                    axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
+                    //axi_rresp <= (axi_rvalid) ? 2'b00 : axi_rresp;
                     axi_bid <= axi_bid;
         end
         default : begin
-                    axi_rvalid <= axi_rvalid;
-                    axi_bvalid <= axi_bvalid;
-                    axi_rresp <= axi_rresp;
-                    axi_bid <= axi_bid;
+                    //axi_rvalid <= axi_rvalid;
+                    axi_bvalid <= 1'b0;
+                    //axi_rresp <= axi_rresp;
+                    axi_bid <= 2'b00;
         end
         endcase
     end
 end
-
-always @(posedge clk) begin
-    if (reset) begin
-        axi_rdata <= 128'h0;
+*/
+always @(posedge clk or negedge reset) begin
+    if (!reset) begin
+        axi_hrdata <= 128'h0;
         axi_bresp <= 2'b00;
     end 
     else begin
         if (axi_rvalid & axi_rready) begin
-            axi_rdata <= hrdata;
+            axi_hrdata <= axi_rdata;
             axi_bresp <= axi_bresp;
         end 
         else if (axi_bvalid & axi_bready) begin
-            axi_rdata <= axi_rdata;
+            axi_hrdata <= axi_hrdata;
             axi_bresp <= axi_bresp;
         end 
         else begin
-            axi_rdata <= axi_rdata;
+            axi_hrdata <= axi_hrdata;
             axi_bresp <= axi_bresp;
         end
+    end
+end
+
+always @(posedge clk or negedge reset) begin
+    if(!reset) begin
+        axi_hresp <= 2'b00;
+    end
+    else begin
+        case(axi_rvalid)
+        1'b0: begin
+            axi_hresp <= axi_hresp;
+        end
+        1'b1: begin
+            axi_hresp <= (axi_rresp == 2'b00) ? 2'b00 : 2'b10;
+        end
+        default: begin
+            axi_hresp <= 2'b00;
+        end
+        endcase
     end
 end
 
